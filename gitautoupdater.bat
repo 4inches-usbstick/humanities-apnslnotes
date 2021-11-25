8f274bd52733e563b1c6f40c712350ca5593fff6
Charles <infoforchacha@gmail.com>
2021-11-25 18:55:27 -0500

    auto commit by update utility at 18:55:27.87 on 25-Nov-21

---
+++ gitautoupdater.bat
@@ -0,0 +1,18 @@
+@echo OFF
+REM robocopy.exe C:\wamp64\www\textengine\sitechats\media\apgovstuff\uploaded C:\wamp64\www\textengine\sitechats\media\apgovstuff\uploaded\git /COPYALL /E
+
+net session>NUL
+if %errorlevel% == 0 ( goto okbypass ) else ( 
+echo Run this file as administrator and try again.
+pause
+exit
+ )
+ 
+:okbypass
+robocopy C:\wamp64\www\textengine\sitechats\media\apgovstuff\uploaded C:\wamp64\www\textengine\sitechats\media\apgovstuff\uploaded\git /COPYALL /E
+rmdir C:\wamp64\www\textengine\sitechats\media\apgovstuff\uploaded\git\git /S /Q
+cd C:\wamp64\www\textengine\sitechats\media\apgovstuff\uploaded\git
+git add *
+git commit -a -S -m "auto commit by update utility at %time% on %date%"
+git push
+pause
